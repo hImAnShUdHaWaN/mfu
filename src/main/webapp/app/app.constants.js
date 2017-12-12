2d952ed769249e5d538114b30d05f25f3c3a162c
Himanshu Dhawan <himanshu.dhawan@gmail.com>
2017-12-13 01:24:06 +0530

    heroku procfile + settings

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('mfuApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', true)
-        .constant('BUILD_TIMESTAMP', "")
+        .constant('DEBUG_INFO_ENABLED', false)
+        .constant('BUILD_TIMESTAMP', 1513105971606)
 ;
 })();
